e6d6dbc4e01e606bd9c63b1a85661ab0679e2619
Shruti Pundir <111945742+ShrutiPundir17@users.noreply.github.com>
2025-02-11 00:24:58 +0530

    Time: 3 ms (80.34%), Space: 9.8 MB (77.46%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     string countAndSay(int n) {
-      string s = "1";
+       string s = "1";
         while (--n) {
-            string t = "";
-            for (int i = 0; i < s.size();) {
-                int j = i;
-                while (j < s.size() && s[j] == s[i]) ++j;
-                t += to_string(j - i);
-                t += s[i];
-                i = j;
+            ostringstream oss;
+            int len = s.size();
+            for (int i = 0; i < len; ) {
+                int count = 1;
+                while (i + count < len && s[i] == s[i + count]) ++count;
+                oss << count << s[i]; // Append count and character
+                i += count;
             }
-            s = t;
+            s = oss.str(); // Convert stream to string
         }
         return s;    
     }
