67c570126dd243d308c62c5215fec2cc17c12f10
Shruti Pundir <111945742+ShrutiPundir17@users.noreply.github.com>
2025-02-10 15:45:32 +0530

    Time: 0 ms (100%), Space: 9.4 MB (77.68%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-     if (num1 == "0" || num2 == "0") return "0";
+        if (num1 == "0" || num2 == "0") {
+            return "0";
+        }
         int m = num1.size(), n = num2.size();
-        vector<int> result(m + n, 0);
+        vector<int> arr(m + n);
         for (int i = m - 1; i >= 0; --i) {
+            int a = num1[i] - '0';
             for (int j = n - 1; j >= 0; --j) {
-                int mul = (num1[i] - '0') * (num2[j] - '0');
-                int sum = mul + result[i + j + 1];
-                result[i + j] += sum / 10;  
-                result[i + j + 1] = sum % 10; 
+                int b = num2[j] - '0';
+                arr[i + j + 1] += a * b;
             }
         }
-        string ans;
-        for (int num : result) {
-            if (!(ans.empty() && num == 0))
-                ans += (num + '0');
+        for (int i = arr.size() - 1; i; --i) {
+            arr[i - 1] += arr[i] / 10;
+            arr[i] %= 10;
         }
-        return ans.empty() ? "0" : ans;    
+        int i = arr[0] ? 0 : 1;
+        string ans;
+        for (; i < arr.size(); ++i) {
+            ans += '0' + arr[i];
+        }
+        return ans;
     }
 };
