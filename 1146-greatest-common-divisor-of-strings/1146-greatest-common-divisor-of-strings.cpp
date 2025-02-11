a250f57c03aee812a73e1f3b65408c3af035203c
Shruti Pundir <111945742+ShrutiPundir17@users.noreply.github.com>
2025-02-11 18:20:56 +0530

    Time: 0 ms (100%), Space: 11.3 MB (15.06%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-     if (str1.size() < str2.size()) return gcdOfStrings(str2, str1);  // Ensure str1 is longer
-
-        if (str1.find(str2) != 0) return "";  // If str2 is not a prefix of str1, return ""
-
-        if (str2.empty()) return str1;  // Base case: when str2 becomes empty, return str1
-
+     if (str1.size() < str2.size()) return gcdOfStrings(str2, str1);  
+        if (str1.find(str2) != 0) return "";
+        if (str2.empty()) return str1;  
         return gcdOfStrings(str1.substr(str2.size()), str2);   
     }
 };
