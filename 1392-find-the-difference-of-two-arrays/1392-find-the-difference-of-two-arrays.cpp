5d644ef2d4d29c240cbf076987a0a003cb13d25b
Shruti Pundir <111945742+ShrutiPundir17@users.noreply.github.com>
2025-02-14 19:25:59 +0530

    Time: 19 ms (74.33%), Space: 36.7 MB (83.13%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
@@ -3,17 +3,23 @@ public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
     unordered_set<int> s1(nums1.begin(), nums1.end());
         unordered_set<int> s2(nums2.begin(), nums2.end());
-        vector<vector<int>> ans(2);
+
+        vector<int> unique1, unique2;
+        unique1.reserve(s1.size());
+        unique2.reserve(s2.size());
+
         for (int v : s1) {
-            if (!s2.contains(v)) {
-                ans[0].push_back(v);
+            if (s2.find(v) == s2.end()) { // Avoids C++20 `.contains()`
+                unique1.push_back(v);
             }
         }
+        
         for (int v : s2) {
-            if (!s1.contains(v)) {
-                ans[1].push_back(v);
+            if (s1.find(v) == s1.end()) {
+                unique2.push_back(v);
             }
         }
-        return ans;    
+
+        return {move(unique1), move(unique2)};   
     }
 };
