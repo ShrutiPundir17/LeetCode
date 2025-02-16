a8b2b6aae4efa8db378f75e5e61bb5e21d217bd4
Shruti Pundir <111945742+ShrutiPundir17@users.noreply.github.com>
2025-02-16 10:45:50 +0530

    Time: 291 ms (14.9%), Space: 129.1 MB (26.96%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
     int minReorder(int n, vector<vector<int>>& connections) {
-    vector<pair<int, int>> g[n];
+    vector<vector<int>> adj(n);
+        unordered_set<string> original_edges;
+        
+        // Construct the adjacency list and track original edges
         for (auto& e : connections) {
             int a = e[0], b = e[1];
-            g[a].emplace_back(b, 1);
-            g[b].emplace_back(a, 0);
+            adj[a].push_back(b);
+            adj[b].push_back(a);
+            original_edges.insert(to_string(a) + "," + to_string(b)); // Store as "a,b"
         }
-        function<int(int, int)> dfs = [&](int a, int fa) {
-            int ans = 0;
-            for (auto& [b, c] : g[a]) {
-                if (b != fa) {
-                    ans += c + dfs(b, a);
+
+        int changes = 0;
+        vector<bool> visited(n, false);
+
+        // BFS approach instead of DFS to avoid recursion overhead
+        queue<int> q;
+        q.push(0);
+        visited[0] = true;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (int neighbor : adj[node]) {
+                if (!visited[neighbor]) {
+                    // If edge was originally (node → neighbor), it needs to be reversed
+                    if (original_edges.count(to_string(node) + "," + to_string(neighbor))) {
+                        changes++;
+                    }
+                    visited[neighbor] = true;
+                    q.push(neighbor);
                 }
             }
-            return ans;
-        };
-        return dfs(0, -1);   
+        }
+
+        return changes;
     }
 };
