030ad00eab9146de9620cad86157ee673f1c920b
Shruti Pundir <111945742+ShrutiPundir17@users.noreply.github.com>
2025-02-18 15:16:06 +0530

    Time: 2 ms (54.41%), Space: 8 MB (38.21%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1, right = n;  
+        while (left < right) {  
+            int mid = left + ((right - left) >> 1);  
+            if (isBadVersion(mid)) {  
+                right = mid; 
+            } else {  
+                left = mid + 1;  
+            }
+        }
+        return left;  
+    }
+};
