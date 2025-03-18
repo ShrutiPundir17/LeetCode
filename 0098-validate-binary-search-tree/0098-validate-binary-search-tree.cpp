9cb7e4d86f0f4e29965e843f06106177610f999e
Shruti Pundir <111945742+ShrutiPundir17@users.noreply.github.com>
2025-03-18 15:44:30 +0530

    Time: 0 ms (100%), Space: 22.1 MB (19.86%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,27 +1,21 @@
-
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
         stack<TreeNode*> st;
         TreeNode* prev = nullptr;
-        
         while (root || !st.empty()) {
             while (root) {
                 st.push(root);
                 root = root->left;
             }
-            
             root = st.top();
             st.pop();
-
             if (prev && prev->val >= root->val) {
                 return false;
             }
-            
             prev = root;
             root = root->right;
         }
-        
         return true;
     }
 };
